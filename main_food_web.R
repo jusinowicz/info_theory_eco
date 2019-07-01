3cbfc8f0e31d57705c986f228182c775a0a48d21
jusinowicz <jusinowicz@gmail.com>
2019-07-01 18:43:14 +0200

    Started a main file for the food web model

---
+++ main_food_web.R
@@ -0,0 +1,8 @@
+#=============================================================================
+# R code to create a simple food web and try to model it using information 
+# theoretic tools. 
+# 1. Create population dynamics for resource, herbivore, and predator. 
+# 2. Change the dynamics by perturbing the resource behavior. 
+# 3. Use information theory to track the resulting changes. 
+#=============================================================================
+
