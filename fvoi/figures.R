648b288f83c0bfdb550806ffee62d626dac51561
jusinowicz <31281754+jusinowicz@users.noreply.github.com>
2021-06-08 10:34:38 -0700

    Switch box 1 and 2, start Fig 1

--- fvoi/figures.R
+++ fvoi/figures.R
@@ -10,39 +10,9 @@ library(tidyverse)
 #=============================================================================
 # Box 1
 #=============================================================================
-load("fvoi_plot1.var")
-###Social info model
-both_long_use = subset(both_long, time <= 60 )
-both_long_use$species[both_long_use$species=="1"] = "species 1, no info"
-both_long_use$species[both_long_use$species=="2"] = "species 2, no info"
-both_long_use$species[both_long_use$species=="3"] = "species 1, social info"
-both_long_use$species[both_long_use$species=="4"] = "species 2, social info"
-
-#For text plotting
-xpos = c(matrix(40,4,1))
-ypos = c(both_long_use$N[both_long_use$time == 60])
-ypos = ypos + (c(-1,1,-1,1))
-suse = unique(both_long_use$species)
-
-p0 =ggplot()+ geom_line( data = both_long_use, aes ( x = time, y = N, color = species)  )+ 
-	geom_text( aes(x = xpos, y = ypos, label = suse, color = suse) ) +
-	ylab("Population")+ xlab("Time")+   scale_colour_viridis_d()+
-	theme_bw() + theme(
-	text = element_text(size=14),
-	panel.border = element_blank(), panel.grid.major = element_blank(),
-	panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
-	legend.position = "none"
-	)
-
-p0
-ggsave(file="fvoi_box1.pdf", p0)
-
-
-#=============================================================================
-# Box 2
-#=============================================================================
 ###Draw species intrinsic fitness curve and plot them overtop the realized 
 ###env distribution: 
+ngens = length(env_fit$env)
 r1 = seq(0,1,length=ngens)
 sp1 = exp(-0.5* ( (r1-env_fit$opt[1])/(env_fit$var[1]) )^2 )
 sp2 = exp(-0.5* ( (r1-env_fit$opt[2])/(env_fit$var[1]) )^2 )
@@ -138,6 +108,38 @@ g= grid.arrange(arrangeGrob(p0,p1, ncol=1, nrow=2),
 
 
 ggsave(file="fvoi_box2.pdf", g)
+
+#=============================================================================
+# Box 2
+#=============================================================================
+load("fvoi_plot1.var")
+###Social info model
+both_long_use = subset(both_long, time <= 60 )
+both_long_use$species[both_long_use$species=="1"] = "species 1, no info"
+both_long_use$species[both_long_use$species=="2"] = "species 2, no info"
+both_long_use$species[both_long_use$species=="3"] = "species 1, social info"
+both_long_use$species[both_long_use$species=="4"] = "species 2, social info"
+
+#For text plotting
+xpos = c(matrix(40,4,1))
+ypos = c(both_long_use$N[both_long_use$time == 60])
+ypos = ypos + (c(-1,1,-1,1))
+suse = unique(both_long_use$species)
+
+p0 =ggplot()+ geom_line( data = both_long_use, aes ( x = time, y = N, color = species)  )+ 
+	geom_text( aes(x = xpos, y = ypos, label = suse, color = suse) ) +
+	ylab("Population")+ xlab("Time")+   scale_colour_viridis_d()+
+	theme_bw() + theme(
+	text = element_text(size=14),
+	panel.border = element_blank(), panel.grid.major = element_blank(),
+	panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"),
+	legend.position = "none"
+	)
+
+p0
+ggsave(file="fvoi_box1.pdf", p0)
+
+
 
 #=============================================================================
 # Box 3
@@ -257,6 +259,12 @@ g=grid.arrange(arrangeGrob(p1,p2, ncol=2, nrow=1, bottom = textGrob("Time",gp = 
 
 ggsave(file="fvoi_box3.pdf",g)
 #cairo_pdf(file="fvoi_box3.pdf")
+
+#=============================================================================
+# Figure 1 -- Conceptual figure using the data from Box 1
+#=============================================================================
+# Work in progress
+
 
 #=============================================================================
 # Figure 2
